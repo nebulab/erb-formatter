23d31add1a34f78afb98a1bd269e1c10f9773487
Elia Schito <elia@schito.me>
2022-03-19 15:36:11 +0100

    Add some fixture-based tests

--- test/erb/test_formatter.rb
+++ test/erb/test_formatter.rb
@@ -10,4 +10,13 @@ class ERB::TestFormatter < Minitest::Test
   def test_simple_tag
     assert_equal("<div>\n  asdf\n</div>\n", ERB::Formatter.format("<div        > asdf    </div>"))
   end
+
+  def test_fixtures
+    Dir["#{__dir__}/../fixtures/*.html.erb"].each do |erb_path|
+      expected_path = erb_path.chomp('.erb') + '.expected.erb'
+
+      # File.write expected_path, ERB::Formatter.format(File.read(erb_path))
+      assert_equal(File.read(expected_path), ERB::Formatter.format(File.read(erb_path)))
+    end
+  end
 end
